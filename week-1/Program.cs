a2987b764691194136f92753d1991e2f1e3ace7e
binbashburns <pro.mattburns@gmail.com>
2025-10-23 18:24:23 -0400

    assignment: pushing week 1 assignment

---
+++ week-1/Program.cs
@@ -0,0 +1,6 @@
+﻿// This program asks for a users name and then says hello!
+Console.WriteLine("What is your name?");
+
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello, " + userName + "!");
