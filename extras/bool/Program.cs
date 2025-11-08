9d3bea19f882c20fb146b989ec622744df303613
binbashburns <pro.mattburns@gmail.com>
2025-11-08 14:12:55 -0500

    other things

---
+++ extras/bool/Program.cs
@@ -0,0 +1,80 @@
+﻿// Booleans
+
+// Console.WriteLine("a" == "a");
+// Console.WriteLine("a" == "A");
+// Console.WriteLine(1 == 2);
+
+// string myValue = "a";
+// Console.WriteLine(myValue == "a");
+
+// string value1 = " a";
+// string value2 = "A ";
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+// Console.WriteLine("a" != "a");
+// Console.WriteLine("a" != "A");
+// Console.WriteLine(1 != 2);
+
+// string myValue = "a";
+// Console.WriteLine(myValue != "a");
+
+// Console.WriteLine(1 > 2);
+// Console.WriteLine(1 < 2);
+// Console.WriteLine(1 >= 1);
+// Console.WriteLine(1 <= 1);
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
+
+
+
+// Conditional Operator
+// <evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
+
+// int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {discount}");
+// Above returns 100, because saleAmount is over 1000.
+
+// int saleAmount = 999;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {discount}");
+// Above returns 50, because saleAmount is under 1000.
+
+// Conditional Operator Challenge (coin flip)
+// Random random = new Random();
+// int sideOfCoin = random.Next(2);
+// Console.WriteLine($"Coin Flip Result: {(sideOfCoin > 0 ? "HEADS" : "TAILS")}");
+
+// Boolean Expressions Challenge
+string permission = "Admin|Manager";
+
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
